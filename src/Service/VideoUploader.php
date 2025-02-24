765d11c3d5026d3878241fb04484b7596879d00b
Adam Mańko <adam.maniek247@gmail.com>
2025-02-24 12:02:00 +0100

    Fix slug is not a string type issue and improve readability

--- src/Service/VideoUploader.php
+++ src/Service/VideoUploader.php
@@ -7,16 +7,26 @@ namespace PrestaShop\Module\ProductVideoLoops\Service;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use PrestaShop\Module\ProductVideoLoops\Repository\ProductVideoRepository;
 use Symfony\Component\String\Slugger\AsciiSlugger;
+use PrestaShop\Module\ProductVideoLoops\Service\LinkBuilderService;
 use PrestaShopException;
 
 class VideoUploader
 {
     private $productVideoRepository;
 
-    public function __construct(ProductVideoRepository $productVideoRepository)
-    {
+    private $slugger;
+
+    private $linkBuilderService;
+
+    public function __construct(ProductVideoRepository $productVideoRepository, 
+        AsciiSlugger $slugger, 
+        LinkBuilderService $linkBuilderService
+    ){
         $this->productVideoRepository = $productVideoRepository;
+        $this->slugger = $slugger;
+        $this->linkBuilderService = $linkBuilderService;
     }
+
     /**
      * Uploads a video file.
      *
@@ -28,37 +38,24 @@ class VideoUploader
      * @param AsciiSlugger $slugger Generates URL-friendly file name
      *
      * @return string Unique and safe final file name
-     * @throws PrestaShopException If invalid format or destination directory cannot be created
      */
-    public function upload(UploadedFile $uploadedFile, AsciiSlugger $slugger): string
+    public function upload(UploadedFile $uploadedFile): string
     {
 
-        $allowedExtensions = 'mp4';
-        $extension = $uploadedFile->guessExtension();
-        //TODO: test this exception
-        if ($extension != $allowedExtensions) {
-            throw new PrestaShopException('Nieprawidłowy format pliku.');
-        }
-
         $originalName = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
-        $safeName = $slugger->slug($originalName);
-        $safeName = mb_substr($safeName, 0, 70);
+        $safeName = $this->slugger->slug($originalName);
+        $safeName = mb_substr((string)$safeName, 0, 70);
+
         $extension = $uploadedFile->guessExtension();
+
         $finalFileName = uniqid() . '-' . $safeName . '.' . $extension;
 
-        $destination = _PS_CORE_IMG_DIR_ . 'videoloops/';
-
-        if (!is_dir($destination)) {
-            @mkdir($destination, 0777, true);
+        $folderPath = $this->linkBuilderService->buildVideoFolderPath();
+        if (!is_dir($folderPath)) {
+            @mkdir($folderPath, 0777, true);
         }
 
-        //TODO: test this exception
-        if (!is_dir($destination) && !mkdir($destination, 0777, true) && !is_dir($destination)) {
-            throw new PrestaShopException('Nie można utworzyć katalogu docelowego.');
-        }
-
-
-        $uploadedFile->move($destination, $finalFileName);
+        $uploadedFile->move($folderPath, $finalFileName);
 
         return $finalFileName;
     }
